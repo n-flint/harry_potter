b37cd075a79d3dd619430e03a2da7ff553a1f8ba
Noah Flint <noah.flint24@gmail.com>
2019-06-18 18:29:31 -0600

    refactored facade to use a service

--- app/facades/hogwarts_facade.rb
+++ app/facades/hogwarts_facade.rb
@@ -5,19 +5,15 @@ class HogwartsFacade
   end
 
   def student_count
-    conn = Faraday.get("http://hogwarts-it.herokuapp.com/api/v1/house/slytherin?api_key=alohamora")
-    students = JSON.parse(conn.body)
-    # require 'pry'; binding.pry
-    count = students['data'][0]['attributes']['students'].count
+    service.total_student_num
   end
 
   def students
-    # Use ENV['HOGWARTS_KEY']
-    conn = Faraday.get("http://hogwarts-it.herokuapp.com/api/v1/house/slytherin?api_key=alohamora")
-    students = JSON.parse(conn.body)['data'][0]['attributes']['students']
-    students.map do |student|
-      Student.new(student)
-    end
+    service.students
   end
 
+  private
+    def service
+      @_service ||= HogwartsService.new(@house)
+    end
 end
