efb0cf7fd3269c039e40793aaad5c5b21afc53cc
Noah Flint <noah.flint24@gmail.com>
2019-06-18 18:10:13 -0600

    created students poro

--- app/facades/hogwarts_facade.rb
+++ app/facades/hogwarts_facade.rb
@@ -5,8 +5,19 @@ class HogwartsFacade
   end
 
   def student_count
-    
+    conn = Faraday.get("http://hogwarts-it.herokuapp.com/api/v1/house/slytherin?api_key=alohamora")
+    students = JSON.parse(conn.body)
+    # require 'pry'; binding.pry
+    count = students['data'][0]['attributes']['students'].count
+  end
 
+  def students
+    # Use ENV['HOGWARTS_KEY']
+    conn = Faraday.get("http://hogwarts-it.herokuapp.com/api/v1/house/slytherin?api_key=alohamora")
+    students = JSON.parse(conn.body)['data'][0]['attributes']['students']
+    students.map do |student|
+      Student.new(student)
+    end
   end
 
 end
